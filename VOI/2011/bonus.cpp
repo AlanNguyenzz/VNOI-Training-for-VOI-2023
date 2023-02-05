fb80f2f89565db57a4056677b0da5e09f332e880
AlanNguyenzz <117288749+AlanNguyenzz@users.noreply.github.com>
2023-02-05 08:11:31 +0700

    Add files via upload

---
+++ VOI/2011/bonus.cpp
@@ -0,0 +1,91 @@
+﻿#include<bits/stdc++.h>
+
+/*__________________________________________________________*/
+
+/*________________________start_typedef_____________________*/
+
+typedef float ft;
+typedef double db;
+typedef long double ld;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef unsigned int uint;
+
+/*_________________________end_typedef______________________*/
+
+/*__________________________________________________________*/
+
+/*_______________________start_define_______________________*/
+
+#define faster ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+#define foru(a, b, c) for(ll a = b; a <= c; ++a)
+#define ford(a, b, c) for(ll a = b; a >= c; --a)
+#define fora(a, b) for(auto &a : b)
+#define all(a) a.begin(), a.end()
+#define sz(s) (ll)(s).size()
+#define __lcm(a, b) (ll)(a * b) / __gcd(a, b)
+#define qsort(a, n) sort(a + 1, a + 1 + n)
+#define qrev(a, n) reverse(a + 1, a + 1 + n)
+#define el '\n'
+#define EL cout << el;
+#define pre(x) cout.precision(x);
+#define maximize(a, b) a = (a > b ? a : b)
+#define minimize(a, b) a = (a < b ? a : b) 
+#define debug(x) cerr << "[ " << #x << " ] : " << x << ' ';
+
+/*_________________________end_define_______________________*/
+
+/*__________________________________________________________*/
+
+/*_________________________start_const______________________*/
+
+const ll oo = LLONG_MAX;
+const ll M = (ll)1e9 + 7;
+const ll MxN = (ll)1e5 + 5;
+const db pi = acos(-1.0);
+
+/*__________________________end_const_______________________*/
+
+/*____________________From_IK30_With_Love_<3________________*/
+
+using namespace std;
+using vll = vector<ll>;
+using pll = pair<ll, ll>;
+using vp = vector<pll>;
+using vv = vector<vll>;
+
+void file() {
+//#ifdef ONLINE_JUDGE
+	//freopen("POINTS.INP", "r", stdin);
+	//freopen("POINTS.OUT", "w", stdout);
+//#endif
+}
+
+void solve() {
+    ll n, k;
+    ll dp[1001][1001];
+    cin >> n >> k;
+    foru(i, 1, n) {
+        foru(j, 1, n) {
+            cin >> dp[i][j];
+            dp[i][j] += dp[i - 1][j] + dp[i][j - 1] - dp[i - 1][j - 1];
+        }
+    }
+    ll ans = 0;
+    foru(i, k, n)
+        foru(j, k, n) {
+        ans = max(ans, dp[i][j] - dp[i - k][j] - dp[i][j - k] + dp[i - k][j - k]);
+    }
+    cout << ans;
+}
+
+int main() {
+	faster;
+	ll test = 1;
+	//cin >> test;
+	while(test--)
+		solve();
+	return 0;
+}
+
+//  グエ・cチャン・ハ・アン <3
